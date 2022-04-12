579f2b0122bb357576f7aaeb0b37b43c1c86600c
Radu Seitan <raducseitan@gmail.com>
2022-04-12 07:36:56 -0700

    Week 7 work

--- ASIN.c
+++ ASIN.c
@@ -8,6 +8,36 @@
 Token *iTk;
 Token *consumedTk;
 
+bool unit();
+bool structDef();
+bool varDef();
+bool typeBase();
+bool arrayDecl();
+bool fnDef();
+bool fnParam();
+bool stm();
+bool stmCompound();
+bool expr();
+bool exprAssign();
+bool exprOr();
+bool exprAnd();
+bool exprEq();
+bool exprRel();
+bool exprAdd();
+bool exprMul();
+bool exprCast();
+bool exprUnary();
+bool exprPostfix();
+bool exprPrimary();
+
+bool exprPostfixAux();
+bool exprMulAux();
+bool exprAddAux();
+bool exprRelAux();
+bool exprEqAux();
+bool exprAndAux();
+bool exprOrAux();
+
 bool consume(int code)
 {
     if (iTk->code == code)
@@ -22,8 +52,10 @@ bool consume(int code)
 // unit: ( structDef | fnDef | varDef )* END
 bool unit()
 {
+    Token *start = iTk;
+
     for (;;)
-    { 
+    {
         if (structDef())
         {
         }
@@ -34,12 +66,72 @@ bool unit()
         {
         }
         else
-            break; 
+            break;
     }
     if (consume(END))
     {
         return true;
     }
+
+    iTk = start;
+    return false;
+}
+
+// structDef: STRUCT ID LACC varDef* RACC SEMICOLON
+bool structDef()
+{
+    Token *start = iTk;
+
+    if (consume(STRUCT))
+    {
+        if (consume(ID))
+        {
+            if (consume(LACC))
+            {
+                for (;;)
+                {
+                    if (varDef())
+                    {
+                    }
+                    else
+                        break;
+                }
+                if (consume(RACC))
+                {
+                    if (consume(SEMICOLON))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    iTk = start;
+    return false;
+}
+
+// varDef: typeBase ID arrayDecl? SEMICOLON
+bool varDef()
+{
+    Token *start = iTk;
+
+    if (typeBase())
+    {
+        if (consume(ID))
+        {
+            if (arrayDecl())
+            {
+            }
+            if (consume(SEMICOLON))
+            {
+                return true;
+            }
+        }
+    }
+
+    iTk = start;
+    return false;
 }
 
 // typeBase: INT | DOUBLE | CHAR | STRUCT ID
@@ -65,11 +157,275 @@ bool typeBase()
         {
             return true;
         }
-        else err("Struct type declaration error");
+        else
+            tkerr(iTk, "Struct type declaration error");
     }
 
     iTk = start;
     return false;
 }
 
-bool fnParam();
+// arrayDecl: LBRACKET CT_INT? RBRACKET
+bool arrayDecl()
+{
+    Token *start = iTk;
+
+    if (consume(LBRACKET))
+    {
+        if (consume(CT_INT))
+        {
+        }
+        if (consume(RBRACKET))
+        {
+            return true;
+        }
+    }
+
+    iTk = start;
+    return false;
+}
+
+// fnDef: ( typeBase | VOID ) ID LPAR ( fnParam (COMMA fnParam)* )? RPAR stmCompound
+bool fnDef()
+{
+    Token *start = iTk;
+
+    if (typeBase() || consume(VOID))
+    {
+        if (consume(ID))
+        {
+            if (consume(LPAR))
+            {
+                if (fnParam())
+                {
+                    for (;;)
+                    {
+                        if (consume(COMMA))
+                        {
+                            if (fnParam())
+                            {
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+                if (consume(RPAR))
+                {
+                    if (stmCompound())
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    iTk = start;
+    return false;
+}
+
+// fnParam: typeBase ID arrayDecl?
+bool fnParam()
+{
+    Token *start = iTk;
+
+    if (typeBase())
+    {
+        if (consume(ID))
+        {
+            if (arrayDecl())
+            {
+            }
+            return true;
+        }
+    }
+
+    iTk = start;
+    return false;
+}
+
+/*stm: stmCompound
+| IF LPAR expr RPAR stm ( ELSE stm )?
+| WHILE LPAR expr RPAR stm
+| FOR LPAR expr? SEMICOLON expr? SEMICOLON expr? RPAR stm
+| BREAK SEMICOLON
+| RETURN expr? SEMICOLON
+| expr? SEMICOLON
+*/
+bool stm()
+{
+    Token *start = iTk;
+
+    if (stmCompound())
+    {
+        return true;
+    }
+
+    if (consume(IF))
+    {
+        if (consume(LPAR))
+        {
+            if (expr())
+            {
+                if (consume(RPAR))
+                {
+                    if (stm())
+                    {
+                        if (consume(ELSE))
+                        {
+                            if (stm())
+                            {
+                            }
+                        }
+
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    if (consume(WHILE))
+    {
+        if (consume(LPAR))
+        {
+            if (expr())
+            {
+                if (consume(RPAR))
+                {
+                    if (stm())
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    if (consume(FOR))
+    {
+        if (consume(LPAR))
+        {
+            if (expr())
+            {
+            }
+            if (consume(SEMICOLON))
+            {
+                if (expr())
+                {
+                }
+                if (consume(SEMICOLON))
+                {
+                    if (expr())
+                    {
+                    }
+                    if (consume(RPAR))
+                    {
+                        if (stm())
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (consume(BREAK))
+    {
+        if (consume(SEMICOLON))
+        {
+            return true;
+        }
+    }
+
+    if (consume(RETURN))
+    {
+        if (expr())
+        {
+        }
+        if (consume(SEMICOLON))
+        {
+            return true;
+        }
+    }
+
+    if (expr())
+    {
+    }
+    if (consume(SEMICOLON))
+    {
+        return true;
+    }
+
+    iTk = start;
+    return false;
+}
+
+// stmCompound: LACC ( varDef | stm )* RACC
+bool stmCompound()
+{
+    Token *start = iTk;
+
+    if (consume(LACC))
+    {
+        for (;;)
+        {
+            if (varDef())
+            {
+            }
+            else if (stm())
+            {
+            }
+            else
+                break;
+        }
+        if (consume(RACC))
+        {
+            return true;
+        }
+    }
+
+    iTk = start;
+    return false;
+}
+
+// expr: exprAssign
+bool expr()
+{
+    Token *start = iTk;
+
+    if (exprAssign())
+    {
+        return true;
+    }
+
+    iTk = start;
+    return false;
+}
+
+// exprAssign: exprUnary ASSIGN exprAssign | exprOr
+bool exprAssign()
+{
+    Token *start = iTk;
+
+    if (exprUnary())
+    {
+        if (consume(ASSIGN))
+        {
+            if (exprAssign())
+            {
+                return true;
+            }
+        }
+    }
+
+    if (exprOr())
+    {
+        return true;
+    }
+
+    iTk = start;
+    return false;
+}
